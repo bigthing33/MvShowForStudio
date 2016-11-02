8d6e34122128a2e2f01ac926a5b693596bac8879
MartinC <martin@o2fun.com>
2016-11-02 15:47:36 +0800

    去掉不必要的常量

--- app/src/main/java/com/cyq/mvshow/other/MyConstants.java
+++ app/src/main/java/com/cyq/mvshow/other/MyConstants.java
@@ -1,64 +1,10 @@
 package com.cyq.mvshow.other;
 
-import android.os.Environment;
-
 public class MyConstants {
 
-    /**
-     * 是否是debug模式
-     */
-    public static final Boolean isDebug = false;
     //每次加载的图片个数
     public static final int PAGE_SIZE = 12;
-
-    /**
-     * 以前的常量
-     */
-    public static final String VERSION_NAME = "VERSION_NAME";
-    public static final String VERSION_CODE = "VERSION_CODE";
-    /**
-     * Unknown network class
-     */
-    public static final int NETWORK_CLASS_UNKNOWN = 0;
-
-    /**
-     * wifi net work
-     */
-    public static final int NETWORK_WIFI = 1;
-
-    /**
-     * "2G" networks
-     */
-    public static final int NETWORK_CLASS_2_G = 2;
-
-    /**
-     * "3G" networks
-     */
-    public static final int NETWORK_CLASS_3_G = 3;
-
-    /**
-     * "4G" networks
-     */
-    public static final int NETWORK_CLASS_4_G = 4;
-
-    public static final String CHAPTER_2_PATH = Environment
-            .getExternalStorageDirectory().getPath()
-            + "/singwhatiwanna/chapter_2/";
-
-    public static final String CACHE_FILE_PATH = CHAPTER_2_PATH + "usercache";
-
-    public static final int MSG_FROM_CLIENT = 0;
-    public static final int MSG_FROM_SERVICE = 1;
-
-    public static final String REMOTE_ACTION = "com.ryg.chapter.action_REMOTE";
-    public static final String EXTRA_REMOTE_VIEWS = "extra_remoteViews";
-    public static final String ExternalStorageDirectory_SEARCHIMAGE = Environment
-            .getExternalStorageDirectory().getPath() + "/searchImage";
-    //最新图片的标签
-    public static final String NEWS_IMAGE = "NEWS_IMAGE";
-    public static final String CLASSIES_IMAGE = "CLASSIES_IMAGE";
-    public static final String RANDOME_IMAGE = "RANDOME_IMAGE";
-    public static final String COLLECT_IMAGE = "COLLECT_IMAGE";
-
+    //是否显示图片，还是显示一张默认的图片
+    public static  Boolean isShowMVFromSkyDog = false;
 
 }
--- app/src/main/java/com/cyq/mvshow/utils/ImageUtils.java
+++ app/src/main/java/com/cyq/mvshow/utils/ImageUtils.java
@@ -14,10 +14,10 @@ import com.facebook.drawee.view.SimpleDraweeView;
 
 public class ImageUtils {
     public static void setImageByUrl(SimpleDraweeView simpleImage, String url) {
-        if (MyConstants.isDebug) {
-            url = "http://imgbbs.heiguang.net/forum/201510/06/104432cjc7c8tx7xxqqkgq.jpg";
-        } else {
+        if (MyConstants.isShowMVFromSkyDog) {
             url = MyUrl.TIANGOU_IMAGE_PATH + url;
+        } else {
+            url = "http://imgbbs.heiguang.net/forum/201510/06/104432cjc7c8tx7xxqqkgq.jpg";
         }
         Uri uri = Uri.parse(url);
         simpleImage.setImageURI(uri);
@@ -31,7 +31,6 @@ public class ImageUtils {
                 .setOldController(simpleImage.getController())
                 //构建
                 .build();
-
         //设置DraweeController
         simpleImage.setController(controller);
     }
